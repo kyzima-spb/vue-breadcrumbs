f44102e3e35c3fdfb546ef24c3f6d6184a48e060
Kirill Vercetti <office@kyzima-spb.com>
2022-02-11 22:25:32 +0300

    Update dependencies

--- examples/vuejs/src/main.js
+++ examples/vuejs/src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
+import Breadcrumbs from '@kyzima-spb/vue-breadcrumbs';
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+Vue.use(Breadcrumbs);
 
 new Vue({
   router,
--- examples/vuejs/src/store/index.js
+++ examples/vuejs/src/store/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { makeBreadcrumbsStore } from '@kyzima-spb/vue-breadcrumbs';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  plugins: [
+    makeBreadcrumbsStore({
+      breadcrumbs: {
+        'User': {text: 'Пользователь'},
+      },
+    }),
+  ],
 })
